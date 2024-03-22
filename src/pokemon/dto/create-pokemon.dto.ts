71e198db0733530cf14d16a239624222e9ed7473
RodrigoSHL <shl.catalan@gmail.com>
2024-03-22 16:10:52 -0300

    FEAT: create dto and entity

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,16 @@
-export class CreatePokemonDto {}
+import { IsNumber, IsPositive, IsString, MinLength, isInt } from "class-validator";
+
+export class CreatePokemonDto {
+
+    @IsString()
+    @MinLength(1)
+    readonly name: string;
+
+    @IsNumber()
+    @IsPositive()
+    readonly no: number;
+
+    @IsString()
+    @MinLength(1)
+    readonly type: string;
+}
