62d7208edb5b221d77abc6858d173bc96d8c2932
RodrigoSHL <shl.catalan@gmail.com>
2024-04-02 01:52:51 -0300

    FEAT: add joi to dependencies, and create joi validation schema

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,6 +1,6 @@
 export const EnvConfig = () => ({
   environment: process.env.NODE_ENV || 'dev',
-  port: process.env.PORT || 3000,
+  port: process.env.PORTO || 3000,
   mongoDB: process.env.MONGO_DB,
-  defaultLimit: process.env.DEFAULT_LIMIT || 7,
+  defaultLimit: +process.env.DEFAULT_LIMIT || 7,
 });
